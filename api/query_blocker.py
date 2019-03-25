805fc6fd08d90cb5f7c51ec82bba93c5d830503e
Jan Friedli <jan.friedli@immerda.ch>
2019-03-25 20:04:04 +0100

    added statement blacklist

---
+++ api/query_blocker.py
@@ -0,0 +1,28 @@
+from flask_restful import abort
+
+
+def check_query_blacklist(sql):
+    # THIS IS NOT MEANT TO BE A SECURITY MECHANISM!
+    # simply for telling users that they aren't allowed to run such statements
+    blacklist = [
+        'delete',
+        'update',
+        'alter',
+        'insert',
+        'create',
+        'drop',
+        'set',
+        'add',
+        'call',
+        'exec',
+        'declare',
+        'option',
+        'read',
+        'read_write',
+        'rename',
+        'replace',
+        'truncate'
+    ]
+    for item in blacklist:
+        if item in sql.lower():
+            abort(401, error="The following keyword is not allowed: " + str(item))
